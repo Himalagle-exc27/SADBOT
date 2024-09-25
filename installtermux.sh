18bfaf185a42ed77c4671ba657debe95819c3bac
Himalaglebruhv27 <slavaseannextgeneration@gmail.com>
2024-09-26 07:47:06 +0800

    Update installtermux.sh

---
+++ installtermux.sh
@@ -0,0 +1,7 @@
+pkg update -y
+pkg upgrade -y
+pkg install aapt python -y
+pip3 install --upgrade pip
+pip3 install pyshodan rich prompt_toolkit
+pkg install wget -y
+wget https://github.com/MasterDevX/Termux-ADB/raw/master/InstallTools.sh && bash InstallTools.sh
